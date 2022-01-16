32ab76bff58fa1518aa2a2dc3fb0e07e6f83e0dc
sahil-zngr <smchaudhari.cse18@itbhu.ac.in>
2022-01-16 17:04:02 +0530

    2nd draft

--- PA0/App.py
+++ PA0/App.py
@@ -2,19 +2,24 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
+#function to encrypt and decrypt
+#both encrypt and decrypt equation is same
+# C = E(p) = 27 - p, here c = ciphertext, p = letter/charcter, here a is 1 and z is 26
 def encdec(text):
     ans = ""
     for c in text:
         if c >= 'a' and c <= 'z':
+            #for lower case letters
             ans += chr( ord('a') + ord('z') - ord(c) )
         elif c >='A' and c <='Z':
+            # for upper case letters
             ans += chr( ord('A') + ord('Z') - ord(c) )
         else:
+            # for any other charaters
             ans += c
     return ans
 
-
+# flask home function
 @app.route('/',methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
